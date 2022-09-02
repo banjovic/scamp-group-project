071cd8d71a93ad3dbaadf826431be6f234e7ec8d
khadija garba <naura1835@gmail.com>
2022-09-02 13:35:29 +0100

    create header component, add custom font size and colors to config file, create routing for app

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./HomePage.scss";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div className="h-screen">HomePage</div>;
 };
 
 export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        "logo-sm": "2.1875rem",
+        "logo-lg": "2.5rem",
+      },
+      colors: {
+        darkGrey: "#2C2C2C",
+        gold: "#FFD0A8",
+        lightGold: "#FFDABB",
+        darkGold: "#D9A57A",
+        grey: "#959595",
+        lightGrey: "#B2B2B2",
+        white: "#FFFFFF",
+        lightBrown: "#DDD5D0",
       },
     },
     plugins: [],
