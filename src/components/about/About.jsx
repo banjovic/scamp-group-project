3c6ddab26f5e0d93f27b2d5b4ca7897c2c35e6ac
sharon-leshan <maleos380@gmail.com>
2022-09-14 11:01:13 +0300

    About component

---
+++ src/components/about/About.jsx
@@ -0,0 +1,322 @@
+import React from 'react';
+import './about.scss';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="preview">
+        <div className="text">
+          <h1>The most modern and efficient way to treat skin.</h1>
+          <p>Radiant, nourished and plumper skin is just 30-minutes away</p>
+        </div>
+        <div className="pic">
+          <img
+            src="https://sokebeauty.com/wp-content/uploads/about.jpg"
+            alt="pic"
+          />
+        </div>
+      </div>
+      <div className="alternating">
+        <div className="pic">
+          <img
+            src="https://sokebeauty.com/wp-content/uploads/soke-eye-mouseover.jpg"
+            alt=""
+          />
+        </div>
+        <div className="info">
+          <div>
+            <h2>Forget about masking — now we Soke.</h2>
+            <p>
+              Soke is unlike any other mask. The super coated hydrogel is
+              carefully formulated to ensure its potent peptides stay at their
+              highest efficacy and soak (Soke) into your skin instead of resting
+              on it. You get all the goods without the slippery and sticky
+              residue.
+            </p>
+            <p>
+              Not only does Soke work fast, but it also works on your schedule.
+              Its incredibly adaptable design seamlessly adheres to your skin
+              without a tacky texture so you can wear it at home or on the go.{' '}
+            </p>
+            <p>
+              Incorporate it into your skincare regime and use as frequently as
+              you desire for immediate and cumulative results. It is truly
+              short-term wear for long-term results.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="alternating ">
+        <div className="info">
+          <div>
+            <h2>We go beyond feel good</h2>
+            <p>
+              Every Soke hydrogel treatment delivers a pampering experience that
+              makes you feel good—only that it does so much more than that.
+            </p>
+            <p>
+              Its unique patent-pending delivery system gets you results fast.
+              Plumping, lifting and nourishing your skin in as little as 30
+              minutes.
+            </p>
+            <p>
+              In our clinical trials, all participants reported their skin felt
+              more hydrated, smoother and softer. 90% said wrinkles and fine
+              lines were less visible.
+            </p>
+            <p>Don’t take our word for it…</p>
+            <button>See for youself</button>
+          </div>
+        </div>
+        <div className="pic">
+          <img
+            src="https://sokebeauty.com/wp-content/uploads/Product-Page_5.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="blockquote">
+        <blockquote>
+          “Thirty minutes later, I removed the mask and WOW! a noticeable
+          difference in the texture of my skin and wrinkling and my decolletage
+          was so much brighter.”
+        </blockquote>
+        <span>- Soko fan</span>
+      </div>
+      <div className="alternating">
+        <div className="info">
+          <div>
+            <h2>I’m Amy Newman Brown, founder and CEO of Soke Beauty</h2>
+            <p>
+              I don’t believe in anti-aging. I believe in pro-aging: while we
+              cannot stop the clock, we all have a say in how we want to age.
+            </p>
+            <p>
+              As the Aesthetic Director for one of North America’s top plastic
+              surgeons and a long-time skincare fanatic, I’ve had the
+              opportunity to try it all (well, almost). At the same time, I have
+              worked with hundreds of patients helping them choose how they want
+              to age with the best and latest non-invasive treatments available.
+            </p>
+            <p>
+              I noticed a lot of those patients were looking to improve and
+              maintain their results post-treatment. Especially for the areas
+              that are most commonly neglected and reflect aging the soonest
+              like the eye contour, decolletage and neck.
+            </p>
+            <p>
+              Having tried countless topicals I knew there had to be a better
+              and less time-consuming way to treat those areas of the body. We
+              are all busy. We simply don’t have the time to sit around with a
+              mask.
+            </p>
+            <p> This is when I had the idea for Soke.</p>
+          </div>
+        </div>
+        <div className="pic">
+          <img
+            src="https://sokebeauty.com/wp-content/uploads/about-amy-new.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="alternating">
+        <div className="pic">
+          <img
+            src="https://sokebeauty.com/wp-content/uploads/home-landing.jpg"
+            alt=""
+          />
+        </div>
+        <div className="info">
+          <div>
+            <p>
+              At the time I didn’t know it was going to be what it is today. I
+              just knew it had to be simple, comfortable to wear, and actually
+              work.
+            </p>
+            <p>
+              After going down a rabbit hole of research and talking to half a
+              dozen people I finally found something that would work.
+            </p>
+            <p>
+              We took the most innovative medical-grade ingredients (currently
+              used by plastic surgeons to accelerate the healing process of the
+              skin) and infused them with a powerful multi-peptide complex to
+              create a true hydrogel treatment mask that tightens, plumps and
+              lifts the skin in as little as 30 minutes.{' '}
+            </p>
+            <p>
+              After two clinical trials that reinforced the results we saw, Soke
+              was born.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="alternating">
+        <div className="info">
+          <div>
+            <p>
+              Everyone can use Soke, no matter your skin type, age or lifestyle.
+              I believe it is never too early or too late to invest in your
+              skin. Use it any time and anywhere, whether you are busy working
+              behind a screen, chasing your kids around, or simply enjoying
+              life.
+            </p>
+            <p>
+              <strong>
+                Soke blends modern with effective in a way no other skincare
+                does. It truly is the solution that I, and I believe the rest of
+                the world, have been waiting for.
+              </strong>
+            </p>
+          </div>
+        </div>
+        <div className="pic">
+          <img
+            src="https://sokebeauty.com/wp-content/uploads/julie-optimized.gif"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="quote">
+        <div>
+          <h3>Who says you can’t get anything done in 30-minutes?</h3>
+          <p>Try Soke today for smoother, tighter and plumper skin.</p>
+          <button id="readyBtn">I'm ready to Soke</button>
+        </div>
+      </div>
+      <div className="demo">
+        <div className="pic">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="pic">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="pic">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="testmonials">
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+        <div className="testmony">
+          <img
+            src="https://i.vimeocdn.com/video/1285630522-f222785de61046b033cb6c803ca3ff6fd39cb542e116d652d?mw=300&mh=375"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="subscribe">
+        <div className="logo">Logo</div>
+        <div className="sub">
+          <p>Subscribe</p>
+          <p>
+            Receive 10% off your first purchase by signing up for updates about
+            new products, promotions and events.
+          </p>
+          <input type="email" name="email" placeholder="Your email" />
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
