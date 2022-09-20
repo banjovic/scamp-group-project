3ab8d784a34eca8abd6bf80fd95c8f57feed0a6b
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2022-09-20 10:51:50 +0100

    reportWebVitals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 import { CartProvider } from "./context/cart/cart.context";
 
